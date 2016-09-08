fd60e2eab74f15cf6d999436433d8f3044093155
lukeschunk <luke.schunk@mx.com>
2016-09-08 16:01:12 -0600

    removed comments

--- practice.js
+++ practice.js
@@ -35,11 +35,11 @@
 
   //Function Invocations Here
 
-// var prius = new Car('Toyota', 'Prius', 2011);
-// var mustang = new Car('Ford', 'Mustang', 2013);
+var prius = new Car('Toyota', 'Prius', 2011);
+var mustang = new Car('Ford', 'Mustang', 2013);
 
-// prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
-// mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
+prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
+mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
 //Hint, you'll need to add a move property and write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
 
